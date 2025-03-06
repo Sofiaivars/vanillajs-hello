2cf1efc15560a41731ec5a7363a108ac8cf5e7aa
Sofiaivars <sofiaivars1@gmail.com>
2025-03-06 09:33:44 +0100

    Generador-excusas: Actividad practica modulo javascript__4geeks

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
-import "bootstrap";
-import "./style.css";
+// import "./bootstrap";
+// import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let first = "My";
+  let who = ["hamster", "mother", "horse", "duck"];
+  let action = ["crushed", "broke", "ate", "canceled"];
+  let what = ["my food", "my phone", "the window", "my t-shirt"];
+  let when = ["at the morning", "last night", "when i was outside", "during my shower"]
+
+  let exc1 = Math.floor(Math.random() * who.length);
+  let exc2 = Math.floor(Math.random() * action.length);
+  let exc3 = Math.floor(Math.random() * what.length);
+  let exc4 = Math.floor(Math.random() * when.length);
+
+
+  // document.querySelector("#excuse").innerHTML = first + " " + who[exc1] + " " + action[exc2] + " " + what[exc3] + " " + when[exc4];
+
+  let excuse = first + " " + who[exc1] + " " + action[exc2] + " " + what[exc3] + " " + when[exc4];
+  document.querySelector("#excuse").innerHTML = excuse;
+
+}
