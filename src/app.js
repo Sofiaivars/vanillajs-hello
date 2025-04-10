8ae6979eb6fa6884921ddacbf012aa0f2cb7c732
Sofiaivars <sofiaivars1@gmail.com>
2025-04-10 08:50:18 +0200

    Generador de excusas_SofiaIvars

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
-import "bootstrap";
-import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let who = ['Mi tortuga', 'El vecino', 'Un fantasma'];
+let action = ['rompio', 'robo', 'se comio'];
+let what = ['las plantas', 'la comida del perro', 'el sofa'];
+let when = ['hace un momento', 'anoche', 'el año pasado'];
+
+  let whoRandom = Math.floor(Math.random() * who.length);
+  let actionRandom = Math.floor(Math.random() * action.length);
+  let whatRandom = Math.floor(Math.random() * what.length);
+  let whenRandom = Math.floor(Math.random() * when.length);
+
+  let excuse = who[whoRandom] + " " + action[actionRandom] + " " + what[whatRandom] + " " + when[whenRandom];
+  document.querySelector("#excuse").innerHTML = excuse;
+     }
+  
+
