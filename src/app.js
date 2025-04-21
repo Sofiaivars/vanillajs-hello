993a2d4c18bcf1ae8c3f1ac3a75b826561b7874c
Sofiaivars <sofiaivars1@gmail.com>
2025-04-21 18:29:05 +0200

    Refactorizacion del generador de excusas

--- src/app.js
+++ src/app.js
@@ -1,18 +1,41 @@
 
-window.onload = function() {
-  //write your code here
-let who = ['Mi tortuga', 'El vecino', 'Un fantasma'];
-let action = ['rompio', 'robo', 'se comio'];
-let what = ['las plantas', 'la comida del perro', 'el sofa'];
-let when = ['hace un momento', 'anoche', 'el año pasado'];
+const actionList = ['rompio', 'robo', 'se comio'];
+const whatList = ['las plantas', 'la comida del perro', 'el sofa'];
+const whenList = ['hace un momento', 'anoche', 'el año pasado'];
+const whoList = ['Mi tortuga', 'El vecino', 'Un fantasma'];
 
-  let whoRandom = Math.floor(Math.random() * who.length);
-  let actionRandom = Math.floor(Math.random() * action.length);
-  let whatRandom = Math.floor(Math.random() * what.length);
-  let whenRandom = Math.floor(Math.random() * when.length);
 
-  let excuse = who[whoRandom] + " " + action[actionRandom] + " " + what[whatRandom] + " " + when[whenRandom];
+const _getRandomPosition = (maxNumber)=>{
+  return (Math.floor(Math.random() * maxNumber));
+};
+
+const _getRandomListItem = (list)=>{
+  const position=  _getRandomPosition(list.length);
+  return list[position];
+};
+
+const _actionRandom = ()=> _getRandomListItem(actionList);
+const _whatRandom = ()=> _getRandomListItem(whatList);
+const _whenRandom = ()=> _getRandomListItem(whenList);
+const _whoRandom = ()=> _getRandomListItem(whoList);
+
+const excuseGenerator = () =>{
+  const action= _actionRandom();
+  const what= _whatRandom();
+  const when= _whenRandom();
+  const who= _whoRandom();
+  const excuse = `${who} ${action} ${what} ${when}`;
+  console.log(excuse);
   document.querySelector("#excuse").innerHTML = excuse;
-     }
   
+};
+const button = document.createElement("button");
+button.textContent = "Generar Excusa";
+button.addEventListener("click", excuseGenerator);
+button.style.backgroundColor = "green";
+button.style.borderRadius = "5px";
+button.style.color = "white";       
+button.style.padding = "10px 20px";
+document.body.appendChild(button);
+
 
